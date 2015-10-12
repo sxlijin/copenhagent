897a09cdd03493cc708090ea01061b8b72e3e82d
Samuel Lijin <sxlijin+github@gmail.com>
2015-10-12 12:44:39 -0500

    Better comments and started restructuring

--- start.py
+++ start.py
@@ -8,14 +8,14 @@ from random import randint
 
 ##### GLOBAL VARIABLES <START> #####
 
-quiet = False
-silent = False
-dump_json_state = False
+QUIET = False
+SILENT = False
+dump_json_state = True
 
-hostname='localhost'
+HOSTNAME='localhost'
 
-# set of all legal commands and parameters
-commands = {
+# set of all legal commands, endpoints, and parameters
+COMMANDS = {
 	'map': {
 		'enter': {},
 		'metro': {'direction':['cw', 'ccw']},
@@ -48,8 +48,9 @@ commands = {
 ##### HELPER FUNCTIONS <START> #####
 
 def dump_json(json_obj):
-	# suppress output when silent==True
-	if silent: return
+	"""Prints human-readable JSON from dict or <requests>."""
+	# suppress output when SILENT==True
+	if SILENT: return
 	try:	
 		# type-check $json_obj
 		if type(json_obj) is dict: 
@@ -66,17 +67,19 @@ def dump_json(json_obj):
 		print json_obj
 
 def api_url(tail):
+	"""Returns full concatenated API URL to poll from path and query."""
 	# sanitize $tail before returning
 	if (tail[0] != '/'): tail = '/' + tail
-	return 'http://' + hostname + ':3000/api' + tail
+	return 'http://' + HOSTNAME + ':3000/api' + tail
 
 def get_api(tail, headers={}):
+	"""Returns <requests> obtained by polling the API."""
 	# construct url
 	get_url = api_url(tail)
 	# retrieve url
 	r = requests.get(get_url, headers=headers)
-	if silent: return r
-	# supress output when silent==True
+	# suppress output when SILENT==True
+	if SILENT: return r
 	print
 	print '[polling API <START>]'
 	print 'GET:', get_url
@@ -88,8 +91,8 @@ def get_api(tail, headers={}):
 
 
 def list_opts(cmd, endpoint, param):
-	# list options for $param in $cmd/$endpoint
-	return '(' + ', '.join([repr(opt) for opt in commands[cmd][endpoint][param]]) + ')'
+	"""Returns list of options (opt1, opt2, ...) for $param in /$cmd/$endpoint."""
+	return '(' + ', '.join([repr(opt) for opt in COMMANDS[cmd][endpoint][param]]) + ')'
 
 ##### HELPER FUNCTIONS <END> #####
 
@@ -100,15 +103,17 @@ def list_opts(cmd, endpoint, param):
 ##### WORKER FUNCTIONS <START> #####
 
 def init_agent(name):
+	"""Creates new agent and returns the corresponding agentToken."""
 	return get_api('/environment/connect?name=' + name).json()[u'agentToken']
 
 def open_sess(sess_id = ''):
+	"""Takes control of agent and spawns shell to issue commands for the agent."""
 	# create header with $agentToken
 	h = {u'agentToken':sess_id}
 	# verify $agentToken by sending a message
 	r = get_api('/environment/agent/say?message=opening python session', headers=h)
 
-	# TODO: implement $silent flag
+	# TODO: implement $SILENT flag
 	# if connection successful
 	if (200 == r.status_code):
 		# acknowledge success and spawn shell
@@ -117,16 +122,20 @@ def open_sess(sess_id = ''):
 		# allow $usi=='exit' to terminate shell
 		while(usi != 'exit'):
 			usi = raw_input('disai> ') # unsafe input !!!!!!!!
+			if (usi == 'exit'): continue
 			try_command(usi, h)
 		# announce shell is closed
 		r = get_api('/environment/agent/say?message=closing python session', headers=h)
 	# if connecting with $sess_id fails, ignore and fail
 	else:
-		print 'failed to connect, status code ' + str(r.status_code)
+		print 'failed to connect: status code {}'.format(r.status_code)
+
+
 
 def try_command(usi, h):
+	"""Polls API according to custom commands, returns received <requests> object."""
 	# ad hoc commands
-	if usi == 'papersoccer win': return papersoccer_win(h)
+	if usi == 'papersoccer win': return ps_win(h)
 	usi_split = usi.split()
 	
 	# repeat command n times
@@ -137,58 +146,78 @@ def try_command(usi, h):
 		return
 	
 	# attempt command
-	if ((len(usi_split) > 0) and (usi_split[0] in commands.keys())):
-		return run_command(usi_split[0], usi_split, h=h)
-	else: 
-		if (usi != 'exit'):
-			print 'command unrecognized, please try again'
+	#if ((len(usi_split) > 0) and (usi_split[0] in COMMANDS.keys())):
+	#	return run_command(usi_split[0], usi_split, h=h)
+	if len(usi_split) > 0: return run_command(usi_split, h=h)
+
+
 	
-
-def run_command(cmd, params, h={}):
-	# set get_flag to False if command syntax is bad
-	get_flag = True
-	if (params[0] == cmd): params = params[1:]
-	if (len(params) == 0):
-		params = raw_input(
-				'enter a ' + cmd + ' command (' + ', '.join(commands[cmd].keys()) + '): '
-				).split()
-	endpoint = params[0]
-	endpoint_url = '/' + cmd + '/' + endpoint + '?'
-	params = {p.split('=')[0]:p.split('=')[1] for p in params[1:] if (len(p.split('=')) == 2)}
-	#print params #DEBUG
-	if endpoint in commands[cmd].keys():
-		if ( (len(params) == 0) and (len(commands[cmd][endpoint]) != 0)):
-		# if no parameters passed to endpoint which requires them
-				for reqd_param in commands[cmd][endpoint]:
-					params[reqd_param] = raw_input(
-						'enter a value for ' + reqd_param +
-						' ' + list_opts(cmd, endpoint, reqd_param) + ': ')
-		if ( (len(params) == len(commands[cmd][endpoint])) and
-				params.keys() == commands[cmd][endpoint].keys() ):
-				endpoint_url += '&'.join([p + '=' + params[p] for p in commands[cmd][endpoint]])
-				for param in params:
-					# if illegal value passed as parameter, fail out
-					if params[param] not in commands[cmd][endpoint][param]:
-						print 'ERROR:', repr(params[param]),
-						print 'not recognized as legal value for', repr(param),
-						print list_opts(cmd, endpoint, param)
-						get_flag &= False
-		else:
-				print 'endpoint', endpoint_url, 'requires params: '
-				for k in commands[cmd][endpoint]:
-					print '\t', k,
-					print list_opts(cmd, endpoint, k)
-				print 'but received params:'
-				for k in params: print '\t', k + '=' + params[k]
-				get_flag &= False
-
+# takes ['map', 'metro', 'direction=cw'] etc as $args
+# refers to 'map metro direction=cw' as:
+# 	- command (map)
+# 	- endpoint (metro)
+# 	- parameter (direction=cw)
+def run_command(args, h={}):
+	"""Parses command, polls API if sound, returns received <requests>."""
+	# verify specified command
+	if args[0] in COMMANDS.keys(): 	args = args[1:]
+	# FAIL OUT if command verification fails
 	else:
-		print cmd, 'command not recognized:', endpoint
-		get_flag &= False
+		print 'command not recognized: {}'.format(args[0])
+		return
+	
+	# if no endpoint given, prompt user for one
+	if len(args) == 0: 
+		args = raw_input( 'enter a {} endpoint ({}): '.format(
+			cmd, ', '.join(COMMANDS[cmd].keys())) ).split()
+	endpoint = args[0]
+	
+	# begin constructing 
+	api_query = '/' + cmd + '/' + endpoint + '?'
+	
+	# parse params into a dict
+	args = { p.split('=')[0]:p.split('=')[1] 
+		for p in args[1:] 
+		if len(p.split('=')) == 2 }
 
-	if get_flag:
-		r = get_api(endpoint_url, headers=h)
-		return r
+	# verify specified endpoint
+	if endpoint in COMMANDS[cmd].keys():
+		# if no parameters passed to endpoint which requires them,
+		# prompt user for parameters
+		if len(args) == 0 and len(COMMANDS[cmd][endpoint]) != 0 :
+			for reqd_param in COMMANDS[cmd][endpoint]:
+				args[reqd_param] = raw_input( 
+					'enter a value for {0} {1}: {0}='.format(
+					param, list_opts(cmd, endpoint, param)))
+
+		# verify specified parameters
+		if args.keys() == COMMANDS[cmd][endpoint].keys() :
+			for param in args:
+				# if illegal value passed as parameter, fail out
+				if args[param] not in COMMANDS[cmd][endpoint][param]:
+					print 'ERROR:', repr(args[param]),
+					print 'not recognized as legal value for', repr(param),
+					print list_opts(cmd, endpoint, param)
+					get_flag &= False
+		# FAIL OUT if parameter verification fails
+		else:
+			print 'endpoint', endpoint_url, 'requires params with legal values: '
+			for k in COMMANDS[cmd][endpoint]:
+				print '\t', k, list_opts(cmd, endpoint, k)
+			print 'but received args:'
+			for k in args: print '\t', k + '=' + args[k]
+			return
+	# FAIL OUT if endpoint verification fails
+	else:
+		print '{} endpoint not recognized: {}'.format(cmd, endpoint)
+		return
+
+	# if no problems encountered while parsing
+	# construct api query and return requests object
+	api_query += '&'.join(
+		['{}={}'.format(p, args[p]) for p in COMMANDS[cmd][endpoint]])
+	r = get_api(api_path, headers=h)
+	return r
 
 ##### WORKER FUNCTIONS <END> #####
 
@@ -210,33 +239,35 @@ win_from = {
 		'papersoccer play direction=ne' ]
 }
 
-def papersoccer_win_from(side, h):
+def ps_force_win_from(side, h):
 	for act in win_from[side]: r = try_command(act, h)
+	# uncomment to pause before leaving the game
+	# raw_input('press enter to leave papersoccer game ')
 	try_command('papersoccer leave', h)
 	return r
 	
-def try_ps_direction(direction, h):
+def ps_play_dir(direction, h):
 	return try_command('papersoccer play direction=' + direction, h)
 
-def papersoccer_win(h):
+def ps_win(h):
 	dirs = ['ne','se']
 	rand2 = randint(0,1)
 
 	r = try_command('papersoccer enter', h)
-	r = try_ps_direction(dirs[rand2], h)
+	r = ps_play_dir(dirs[rand2], h)
 	# try a random direction
 	if len(r.json()['action']['percepts']) == 2:
 		rand2 -= 1
-		for i in range(2): r = try_ps_direction(dirs[rand2], h)
+		for i in range(2): r = ps_play_dir(dirs[rand2], h)
 		if len(r.json()['action']['percepts']) == 2:
 			rand2 -= 1
-			r = try_ps_direction(dirs[rand2], h)
-			r = try_ps_direction('e', h)
-			r = papersoccer_win_from(
+			r = ps_play_dir(dirs[rand2], h)
+			r = ps_play_dir('e', h)
+			r = ps_force_win_from(
 				r.json()['action']['percepts'][0][0], h)
 	if r.json()['action']['percepts'] == ['w']:
-		r = papersoccer_win_from(dirs[rand2][0], h)
-	if not silent: print '\n', r.json()['action']['message']
+		r = ps_force_win_from(dirs[rand2][0], h)
+	if not SILENT: print '\n', r.json()['action']['message']
 
 ##### ACTION FUNCTIONS <END> #####
 
