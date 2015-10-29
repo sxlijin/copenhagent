ec9cb4b2d47e7420760ebc934e2e40bb96154e5c
Samuel Lijin <sxlijin+github@gmail.com>
2015-10-29 13:19:14 -0500

    improved log parser

--- runtime_log_parser.py
+++ runtime_log_parser.py
@@ -4,7 +4,27 @@
 
 import sys
 
+runtime_sum = {}
+runtime_count = {}
+
 for fname in sys.argv[1:]:
     f = open(fname, 'rU')
-    l = [float(line.strip()[-10:]) for line in f if 'nav_solve() runtime' in line]
-    print fname, str(1.0*sum(l)/len(l))
+
+    for line in f:
+        if 'runtime' not in line: continue
+
+        line = line.split(' : ')[1]
+        line = line.split()
+
+        (fxn, runtime) = (line[0], float(line[-1]))
+
+        if fxn not in runtime_sum:
+            runtime_sum[fxn] = 0.0
+            runtime_count[fxn] = 0
+
+        runtime_sum[fxn] += runtime
+        runtime_count[fxn] += 1
+
+    for fxn in runtime_sum: 
+        print '%10.10s : %15.15s had average runtime %12.8f ms (%4d trials)' % (
+            fname, fxn, runtime_sum[fxn]/runtime_count[fxn], runtime_count[fxn])
