5abf86de195aa116eaf687b1abf2bb41512ae897
Samuel Lijin <sxlijin+github@gmail.com>
2015-11-03 17:45:31 -0600

    disabled automatic auton

--- ai/auton.py
+++ ai/auton.py
@@ -24,8 +24,9 @@ def best_dest(r):
             for loc in locs if 'activities' in locs[loc] and
                                 'navigation' in locs[loc]['activities'])
 
-while (agent.n_actions < 5000):
-    for cmd in best_path_from_to[agent.location][best_dest(r)[-1]][-1]:
-        shell.try_command(cmd)
-    shell.try_command('navigation ai')
-    r = shell.try_command('navigation leave')
+def simple_auton():
+    while (agent.n_actions < 5000):
+        for cmd in best_path_from_to[agent.location][best_dest(r)[-1]][-1]:
+            shell.try_command(cmd)
+        shell.try_command('navigation ai')
+        r = shell.try_command('navigation leave')
