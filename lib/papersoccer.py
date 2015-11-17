340bd97d7384ee9f2881553e101c68c4e1cb17e9
Samuel Lijin <sxlijin+github@gmail.com>
2015-11-17 10:36:16 -0600

    created SearchNode() class for search algs
    SearchNodes to be created in the process of generating nodes during
    a search algorithm to minimize node expansion cost.

--- lib/papersoccer.py
+++ lib/papersoccer.py
@@ -19,25 +19,16 @@ class Vertex:
         ps_json_vertex = ps_vertices[self.vertex_key]
         self.ps_row = ps_json_vertex['row']
         self.ps_column = ps_json_vertex['column']
+        self.move_again = ps_json_vertex.get('visited', False)
+        self.is_terminal = ps_json_vertex['type'] == 'terminal'
 
+        # store the neighbors as <dir>:<Vertex> pairs
+        # initialize as <dir>:<key> pairs; Graph() finishes construction
         self.neighbors = {}
         for d in self.dirs:
             neighbor = {   'row'   :   self.get_row() + self.dirs[d][0],
                            'column':   self.get_column() + self.dirs[d][1] }
             self.neighbors[d] = '[{row},{column}]'.format(**neighbor)
-
-
-#unk#        self.next_vertices = {}
-#unk#        for d in self.dirs:
-#unk#            if d in ps_json_vertex: continue # skip if edge marked
-#unk#
-#unk#            successor = {   'row'   :   self.get_row() + self.dirs[d][0],
-#unk#                            'column':   self.get_column() + self.dirs[d][1] }
-#unk#            successor_key = '[{row},{column}]'.format(**successor)
-#unk#            if successor_key in ps_vertices:
-#unk#                self.next_vertices[successor_key] = d
-
-        self.move_again = ps_json_vertex.get('visited', False)
 
     def __str__(self):
         return self.vertex_key
@@ -86,60 +77,32 @@ class Graph:
         """Parse the JSON to save the graph of Papersoccer.Instance."""
         self.ps_json_graph = ps_json_graph
         
+        # store vertices of papersoccer graph
         self.vertex_table = {
             vertex_key:Vertex(self, ps_json_graph, vertex_key)
             for vertex_key in ps_json_graph['vertices'] 
         }
-
+        
+        # properly populate the $neighbors dicts with <Vertex>s as values
         for vertex in self.vertex_table.viewvalues():
-            vertex.neighbors = {   self.vertex_table[key] 
-                                        for key in vertex.neighbors 
-                                        if key in self.vertex_table     }
+            vertex.neighbors = {    item[0]:self.get_vertex(item[1])
+                                    for item in vertex.neighbors.viewitems()
+                                    if item[1] in self.vertex_table     }
 
-        #self.visited_table = { vertex:{} for vertex in self.vertex_table }
-        self.visited_table = ps_json_graph['edges']
-
+        # record visited edges as nested dict
+        # if x in edge_table[y], then y in edge_table[x] and x-y was visited
+        edges = ps_json_graph['edges']
+        self.edge_table = { self.get_vertex(tail)
+                            :
+                            { self.get_vertex(head) : edges[tail][head]
+                              for head in edges[tail]
+                            }
+                            for tail in edges   }
 
           
     def get_vertex(self, vertex_key):
         """Return the <Vertex> corresponding to $vertex_key."""
         return self.vertex_table[vertex_key]
-
-    def get_nexts(self, vertex):
-        # $nexts should be a list of lists of moves
-        nexts = []
-        #neighbors = [   self.vertex_table[key]
-        #                for key in vertex.neighbors.viewvalues()
-        #                if key in self.vertex_table     ]
-#
-        frontier = structs.Queue()
-        for key in vertex.neighbors.viewvalues():
-            try:    frontier.add( [self.vertex_table[key]] )
-            except KeyError: pass
-
-        while not frontier.is_empty():
-            next_seq = frontier.rm()
-            if next_seq[-1].move_again:
-                pass
-                # ???
-                # problem is same as below:
-                # how do you consider moves that have been made earlier in the sequence?
-                # maybe it would be better to adjust minimax so that it uses next.move_again
-            else:
-                nexts.append( next_seq )
-
-        
-        for key in vertex.neighbors.viewvalues():
-
-            if key not in self.vertex_table: continue
-            neighbor = self.vertex_table[key]
-            if neighbor.move_again:
-                ### below has infinite recursion problem
-                #nexts.extend(   [neighbor].extend(next_seq) 
-                #                for next_seq in self.get_nexts(neighbor)    )
-            else:
-                nexts.append( [neighbor] )
-        return nexts       
 
 
 class Instance:
@@ -159,12 +122,19 @@ class Instance:
         # begin parsing the FullResponse
         ps_setup = r.json()['state']['papersoccer'][self.token]
         ps_config = ps_setup['config']
-        self.graph = Graph(ps_setup['soccerfield'])
+        ps_field = ps_setup['soccerfield']
+        self.graph = Graph(ps_field)
         
         # store $seed, which is the default weight for nav vertices
         self.seed = ps_config['seed']
         self.curr_vertex = self.graph.get_vertex(
             '[{row},{column}]'.format(**ps_setup['currentVertex']))
+        self.curr_vertex.move_again = True
+        self.n_moves = 0
+
+        self.goal_vertex = self.graph.get_vertex(
+            '[{row},{column}]'.format(  row = ps_field['height']/2,
+                                        column = ps_field['width']-1   ))
 
     def get_current(self): 
         """Returns <Vertex> specified to be initial position."""
@@ -174,12 +144,91 @@ class Instance:
         """Return the seed for the NavInst."""
         return self.seed
 
+    def check_traversed(self, tail, head):
+        """Return boolean corresp. to whether ($tail, $head) is "traversed"."""
+        return head in self.graph.edge_table[tail]
+
+    def check_visited(self, vertex):
+        """Return boolean corresp. to whether $vertex has been "visited"."""
+        return vertex.move_again
+
     def update_with_dir(self, direction):
         """Update the Instance and Graph by moving the ball in a given direction."""
+        self.n_moves += 1
         dest_vertex = self.get_current().get_next_via(direction)
-        graph.visited_table[curr_vertex][dest_vertex] = 'visited'
-        graph.visited_table[dest_vertex][curr_vertex] = 'visited'
+        if self.debug:
+            print 'went %s from %s to %s' % (direction, self.get_current(), dest_vertex)
+        self.graph.edge_table[self.curr_vertex][dest_vertex] = 'visited'
+        self.graph.edge_table[dest_vertex][self.curr_vertex] = 'visited'
         self.curr_vertex = dest_vertex
+        self.curr_vertex.move_again = True
+        
+class SearchNode:
+    """The Nodes for search algorithms to use."""
+
+    #def __init__(self, prev_node, curr_search, searched):
+    def __init__(self, prev_node, curr_vertex, moves):
+        self.prev_node = prev_node  # point to the preceding search node
+        self.curr_vertex = curr_vertex
+        self.moves = moves
+        #self.curr_search = curr_search  # save the path of the current search
+        #self.curr_vertex = self.curr_search[-1] # DANGEROUS: makes jumps possible
+
+        prev_vertex = self.prev_node.get_current()
+        self.visited = (    (prev_vertex, self.curr_vertex), 
+                            (self.curr_vertex, prev_vertex)     )
+        
+        #searched[prev_vertex][self.curr_vertex] = self.curr_search
+        #searched[self.curr_vertex][prev_vertex] = self.curr_search
+
+        #try:
+        #    self.curr_vertex = self.curr_vertex.get_next_via(dir_from_prev)
+        #except KeyError:
+        #    pass # if $dir_from_prev is None, for initial SearchNode
+
+    def get_current(self):
+        """Return the vertex corresponding to this node of the search."""
+        return self.curr_vertex
+
+    def check_traversed(self, tail, head):
+        """Return boolean corresp. to whether (tail, head) edge traversed."""
+        return ((tail, head) in self.visited or 
+                self.prev_node.check_traversed(tail, head)
+                )
+
+    def check_visited(self, vertex=None):
+        """Return boolean corresp. to whether $vertex has been visited."""
+        if vertex == None: vertex = self.get_current()
+        return (self.get_current() == vertex or 
+                self.prev_node.check_visited(vertex)
+                )
+
+    def get_next(self, direction):
+        """Return the successor SearchNode corresp. to $direction."""
+        return SearchNode(  self, 
+                            self.curr_vertex.get_next_via(direction), 
+                            self.moves+1)
+
+    def get_nexts(self):
+        """Return the successor SearchNodes."""
+        return tuple(   SearchNode(self, vertex, self.moves+1)
+                        for vertex in self.curr_vertex.neighbors.viewvalues()
+                        if not self.check_traversed(self.get_current(), vertex)
+                        )
+
+    def show_nexts(self):
+        """Graphically show the grid from current to the successors."""
+        
+        nexts = [nx.get_current() for nx in self.get_nexts()]
+
+        grid = list(self.get_current().neighbors.values())
+        grid.append(self.get_current())
+        grid = sorted(grid, key=str)
+        grid = [pt if pt in nexts else '-'*5 for pt in grid]
+
+        print '\n +-- nexts of %s are:\n |' % self.get_current()
+        print ('\n | %5s   %5s   %5s\n |'*(len(grid)/3))[1:] % tuple(grid)
+
         
     
 class Agent:
@@ -191,7 +240,7 @@ class Agent:
         self.debug = debug
     
     def __str__(self):
-        return self.get_pos()
+        return self.instance.get_current()
     
     def alg_log_start(self, alg_name):
         """Log the start of a papersoccer algorithm."""
@@ -211,24 +260,15 @@ class Agent:
         if type(moves) in (str, unicode):
             r = self.instance.try_command(
                 'papersoccer play direction=%s' % moves)
-            if r.status_code == 200:
-                for d in [moves].extend(r.json()['percepts']):
+            if r.status_code == 200 and r.json()['action']['applicable']:
+                print '\n(me)  %s ->  ' % moves,
+                print '(cpu) ',
+                for move in r.json()['action']['percepts']: print move, ' -> ',
+                print '\n'
+                for d in [moves] + r.json()['action']['percepts']:
                     self.instance.update_with_dir(d)
             return r
-        # if passed list of moves
-        elif type(moves) is list:
-            # if moves are in the form of ($direction, $dest_vertex)
-            if type(moves[0]) is tuple:
-                # move in $direction
-                for move in moves: self.cmd_ps_move(move[0])
-            # if moves are in the form of $direction
-            elif type(moves[0]) is str:
-                # move in $direction
-                for move in moves: self.cmd_ps_move(move)
-    
+
     def cmd_ps_leave(self):
         """Command agent to leave papersoccer."""
         return self.instance.try_command('papersoccer leave')
-
-    def stupid(self):
-        """Stupid try e then ne then se."""
