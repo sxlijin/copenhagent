1339f7b2a827c624e73b646ad95153854783fa9b
Samuel Lijin <sxlijin+github@gmail.com>
2015-11-20 08:51:13 -0600

    must specify hostname to run ai/auton
    added argparse to ai/auton and implemented hostname as required arg

--- ai/auton.py
+++ ai/auton.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import sys, time
+import sys, time, argparse
 import bestpaths
 try:
     import shell
@@ -10,14 +10,7 @@ except ImportError:
         'user@machine:.../copenhagent$ python -m ai.auton'
         ))
 
-HOSTNAME = 'localhost'
-
-shell = shell.Shell(name='auton', hostname=HOSTNAME)
-agent = shell.active_agent
-
-r = shell.try_command('map enter')
-
-best_path_from_to = bestpaths.best_paths(r)
+best_path_from_to = None
 
 def best_dest(r):
     locs = r.json()['state']['map']['locations']
@@ -26,8 +19,9 @@ def best_dest(r):
             for loc in locs if 'activities' in locs[loc] and
                                 'navigation' in locs[loc]['activities'])
 
-def simple_auton():
-    global r
+def simple_auton(shell, r):
+    # hops around navigation activities to win
+    agent = shell.active_agent
     while (agent.n_actions < 5000):
         while True:
             b = best_dest(r)
@@ -38,4 +32,32 @@ def simple_auton():
             shell.try_command(cmd)
         r = shell.try_command('navigation ai')
 
-# simple_auton()
+def main():
+    parser = argparse.ArgumentParser(
+        description = ' '.join((
+            'Initializes an autonomous agent in the copenhagent environment.',
+            ''
+        ))
+    )
+
+    parser.add_argument(
+        '-hostname',
+        metavar='<hostname>',
+        required=True,
+        help='hostname of the copenhagent server to connect to')
+    
+    hostname = parser.parse_args().hostname
+
+    s = shell.Shell(name='auton', hostname=hostname)
+    agent = s.active_agent
+    
+    r = s.try_command('map enter')
+    
+    global best_path_from_to
+    best_path_from_to = bestpaths.best_paths(r)
+    simple_auton(s, r)
+    
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
